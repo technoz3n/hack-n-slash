bb21e0cf69874e8ab64ebbce35d2a34119588296
technoz3n <daboss123517323@gmail.com>
2026-09-22 19:02:03 -0700

    haii

---
+++ src/client/java/xyz/technoz3n/hacknslash/client/HackNSlashClient.java
@@ -0,0 +1,10 @@
+package xyz.technoz3n.hacknslash.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HackNSlashClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
